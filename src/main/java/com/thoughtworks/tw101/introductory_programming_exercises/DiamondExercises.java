e17f98cbdc5b9ab5838417a6de51d7b1772ba955
Brian Sayler <sayler.b@gmail.com>
2017-04-14 16:03:05 -0500

    refactor diamond method to handle names

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
@@ -5,7 +5,7 @@ import static java.lang.Math.abs;
 public class DiamondExercises {
     public static void main(String[] args) {
         drawAnIsoscelesTriangle(3);
-        drawADiamond(8);
+        drawADiamond(8, null);
         drawADiamondWithYourName(3);
     }
 
@@ -38,7 +38,8 @@ public class DiamondExercises {
 //            *****
 //             ***
 //              *
-    private static void drawADiamond(int n) {
+    private static void drawADiamond(int n, String name) {
+
         Integer totalLines = (n * 2) - 1;
         Integer middleRowIndex = (totalLines - 1) / 2;
 
@@ -51,13 +52,21 @@ public class DiamondExercises {
                 System.out.print(" ");
             }
 
-            for (int starIndex = 0; starIndex < starTotal; starIndex++) {
-                System.out.print("*");
+            if (rowIndex == middleRowIndex && !isBlank(name)) {
+                System.out.print("Brian");
+            } else {
+                for (int starIndex = 0; starIndex < starTotal; starIndex++) {
+                    System.out.print("*");
+                }
             }
 
             System.out.println();
         }
 
+    }
+
+    private static boolean isBlank(String string) {
+        return string == null || "".equals(string);
     }
 
 //    Diamond with Name
@@ -69,6 +78,6 @@ public class DiamondExercises {
 //            ***
 //             *
     private static void drawADiamondWithYourName(int n) {
-
+        drawADiamond(n, "Brian");
     }
 }
