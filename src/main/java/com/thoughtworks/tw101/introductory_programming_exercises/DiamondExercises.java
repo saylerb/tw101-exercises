f921f5492a104a93da876456ffb59462f12b54bc
Brian Sayler <sayler.b@gmail.com>
2017-04-13 20:07:15 -0500

    solution for diamond

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
@@ -1,5 +1,7 @@
 package com.thoughtworks.tw101.introductory_programming_exercises;
 
+import static java.lang.Math.abs;
+
 public class DiamondExercises {
     public static void main(String[] args) {
         drawAnIsoscelesTriangle(3);
@@ -33,6 +35,23 @@ public class DiamondExercises {
 //             ***
 //              *
     private static void drawADiamond(int n) {
+        Integer totalLines = (n * 2) - 1;
+        Integer middleRowIndex = (totalLines - 1) / 2;
+
+        for (int i = 0; i < totalLines; i++) {
+
+            Integer padding = abs(middleRowIndex - i);
+
+            for (int num = 0; num < padding; num++) {
+                System.out.print(" ");
+            }
+
+            for (int asterisks = 0; asterisks < totalLines - (padding * 2); asterisks++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
 
     }
 
