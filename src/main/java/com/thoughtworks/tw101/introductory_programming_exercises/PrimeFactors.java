9e4ca65a332b70c25ee191357d16a8873d47cdd3
Brian Sayler <sayler.b@gmail.com>
2017-04-15 11:41:54 -0500

    add prime factorization solution
    - added some more examples
    - wrote solution to collect the prime factors

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
@@ -6,14 +6,41 @@ package com.thoughtworks.tw101.introductory_programming_exercises;
 //
 //  For example, generate(1) should return an empty list and generate(30) should return the numbers: 2,3,5.
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import static java.lang.Math.sqrt;
+
 public class PrimeFactors {
+
     public static void main(String[] args) {
-        List<Integer> primeFactors = generate(30);
+        List<Integer> primeFactors = generate(1);
+        System.out.println(Arrays.toString(primeFactors.toArray()));
+
+        primeFactors = generate(30);
+        System.out.println(Arrays.toString(primeFactors.toArray()));
+
+        primeFactors = generate(75);
+        System.out.println(Arrays.toString(primeFactors.toArray()));
     }
 
     private static List<Integer> generate(int n) {
-        return null;
+        List<Integer> result = new ArrayList<Integer>();
+
+        if (n % 2 == 0 && n > 0) {
+            result.add(2);
+        }
+
+       for (int i = 3; i < sqrt(n); i = i + 2) {
+            int num = n;
+
+            while (num % i == 0) {
+                result.add(i);
+                num = num / i;
+            }
+       }
+
+        return result;
     }
 }
